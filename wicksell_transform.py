5caa49f2d4968671ce3cc598a5add105545e498b
Dorian Depriester <dorian@crans.org>
2020-07-02 11:35:39 +0200

    Take advantage of inherited public methods pdf() and cdf(), with implementation of arg_check methods (taken from basedist attribute)

--- wicksell_transform.py
+++ wicksell_transform.py
@@ -14,17 +14,35 @@ from numbers import Number as num
 class wickselled_trans(stats.rv_continuous):
     "Wicksell's transform of a mixture of two normal distributions"
     
-    def __init__(self, basedist, *args, nint=500, eps=1e-3, **kwargs):
+    def __init__(self, basedist, nint=500, eps=1e-3, **kwargs):
         self.basedist = basedist
         self.nint = nint
         self.eps = eps
         new_name = 'Wicksell''s transform of {}'.format(basedist.name)
-        super().__init__(*args, shapes=basedist.shapes, a=max(0.0, basedist.a), b=basedist.b, name=new_name, **kwargs)
+        if basedist.shapes is None:
+            shapes = 'baseloc, basescale'
+        else:
+            shapes = basedist.shapes + ', baseloc, basescale'
+        super().__init__(shapes=shapes, a=max(0.0, basedist.a), b=basedist.b, name=new_name, **kwargs)
+        self._wicksellvec = np.vectorize(self._wicksell_single, otypes='d')
 
     def _argcheck(self, *args):
-        return self.basedist._argcheck(*args)
+        return self.basedist._argcheck(*args[:-2])
+
+    def _penalized_nnlf(self, theta, x):
+        return self.basedist._penalized_nnlf(theta[:-2], x)
+
+    def _parse_args(self, * args, **kwargs):
+        return self.basedist._parse_args(*args)
+
+    def _parse_args(self, * args, **kwargs):
+        return self.basedist._parse_args_rvs(*args)
+
+    def _reduce_func(self, args, kwds):
+        args += (0.0, 1.0)
+        return self.basedist._reduce_func(args, kwds)
         
-    def _wicksell(self, x, *args, **kwargs):
+    def _wicksell_single(self, x, *args, **kwargs):
         """
         Analytical computation of the Probability Density Function
         """
@@ -35,56 +53,64 @@ class wickselled_trans(stats.rv_continuous):
         else:
             return 0.0
         
-    def _pdf(self, x, *args, **kwargs):
+    def _pdf(self, x, *args):
+        *args, baseloc, basescale = args
         if isinstance(x, num) or x.size == 1:
-            return self._wicksell(x, *args, **kwargs)
+            return self._wicksellvec(x, *args, loc=baseloc, scale=basescale)
         else:
             if x.size < self.nint:
-                return [self._wicksell(xi, *args, **kwargs) for xi in x]
+                return [self._wicksellvec(xi, *args, loc=baseloc, scale=basescale) for xi in x]
             else:
-                x1 = self.basedist.isf(self.eps, *args, **kwargs)
+                loc0 = baseloc[0]
+                scale0 = basescale[0]
+                args = [args_i[0] for args_i in args]
+                x1 = self.basedist.isf(self.eps, *args, loc=loc0, scale=scale0)
                 x0 = 0.999 * x1
-                y0 = self._wicksell(x0, *args, **kwargs)
-                y1 = self._wicksell(x1, *args, **kwargs)
+                y0 = self._wicksellvec(x0, *args, loc=loc0, scale=scale0)
+                y1 = self._wicksellvec(x1, *args, loc=loc0, scale=scale0)
                 fp = (y1 - y0) / (x1 - x0)
                 a = fp / y1            
                 xp = np.linspace(np.min(x), np.max(x), self.nint)
-                yp = [self._wicksell(xi, *args, **kwargs) for xi in xp]
+                yp = [self._wicksellvec(xi, *args, loc=loc0, scale=scale0) for xi in xp]
                 y = np.zeros(x.shape)
                 y[x <= x1] = np.interp(x[x <= x1], xp, yp)
                 y[x > x1] = y1 * np.exp(a * (x[x > x1] - x1))
                 return y
     
-    def _cdf(self, x, *args, **kwargs):
-        integrand = lambda r: self._wicksell(r, *args, **kwargs)
+    def _cdf(self, x, *args):
+        *args, baseloc, basescale = args
+        integrand = lambda r: self._wicksellvec(r, *args, loc=baseloc, scale=basescale)
         if isinstance(x, num) or x.size == 1:
             return integrate.quad(integrand, 0, x)[0]
         else:
             if x.size < 0.0:
                 return [integrate.quad(integrand, 0, xi)[0] for xi in x]
             else:
+                loc0 = baseloc[0]
+                scale0 = basescale[0]
+                args = [args_i[0] for args_i in args]
                 xint = np.linspace(0, np.max(x), self.nint)
-                pdfi = [self._wicksell(xi, *args, **kwargs) for xi in xint]
+                pdfi = [self._wicksellvec(xi, *args, loc=loc0, scale=scale0) for xi in xint]
                 yint = integrate.cumtrapz(pdfi, x=xint, initial=0.0)
                 return np.interp(x, xint, yint, left=0.0, right=1.0)
     
-    def expect(self, *args, **kwargs):
-        integrand = lambda x: self._wicksell(x, *args, **kwargs) * x
+    def expect(self, *args, baseloc=0.0, basescale=1.0, **kwargs):
+        integrand = lambda x: self._wicksellvec(x, *args, loc=baseloc, scale=basescale, **kwargs) * x
         return integrate.quad(integrand, 0, np.inf)[0]
     
-    def mean(self, *args, size=10000, **kwargs):
+    def mean(self, *args, baseloc=0.0, basescale=1.0, size=10000, **kwargs):
         data = self.rvs(size=size, *args, **kwargs)
         return np.mean(data)
         
-    def _ppf(self, p, *args, **kwargs):
-        ppf_0 = self.basedist.ppf(p, *args, **kwargs)
-        return opti.newton_krylov(lambda x: self._cdf(x, *args, **kwargs)-p, ppf_0)  
+    def _ppf(self, p, *args, baseloc=0.0, basescale=1.0, **kwargs):
+        ppf_0 = self.basedist.ppf(p, *args, loc=baseloc, scale=basescale, **kwargs)
+        return opti.newton_krylov(lambda x: self._cdf(x, *args, loc=baseloc, scale=basescale, **kwargs)-p, ppf_0)
 
-    def isf(self, p, *args, **kwargs):
-        isf_0 = self.basedist.isf(p, *args, **kwargs)
-        return opti.newton_krylov(lambda x: self._cdf(x, *args, **kwargs)+p-1, isf_0)
+    def isf(self, p, *args, baseloc=0.0, basescale=1.0, **kwargs):
+        isf_0 = self.basedist.isf(p, *args, loc=baseloc, scale=basescale, **kwargs)
+        return opti.newton_krylov(lambda x: self._cdf(x, *args, loc=baseloc, scale=basescale, **kwargs)+p-1, isf_0)
     
-    def rvs(self, *args, size=None, **kwargs):
+    def rvs(self, *args, baseloc=0.0, basescale=1.0, size=None, **kwargs):
         if size is None:
             n_req = 1
             init_size = 1000
@@ -94,7 +120,7 @@ class wickselled_trans(stats.rv_continuous):
                 init_size = 1000
             else:
                 init_size = n_req
-        r = self.basedist.rvs(*args, size=init_size, **kwargs)
+        r = self.basedist.rvs(*args, size=init_size, loc=baseloc, scale=basescale, **kwargs)
         x_ref = np.cumsum(2*r) - r
         x_pick = np.random.rand(init_size) * np.sum(2*r)
         i = [np.argmin((x_pick_i-x_ref)**2 - r**2) for x_pick_i in x_pick]
@@ -106,14 +132,19 @@ class wickselled_trans(stats.rv_continuous):
         else:
             return np.sqrt(r2).reshape(size)
 
-    def pdf(self, x, *args, **kwargs):
-        return self._pdf(x, *args, **kwargs)
+    # def pdf(self, x, *args, **kwargs):
+    #     baseloc=kwargs.get('baseloc', 0.0)
+    #     basescale=kwargs.get('basescale', 1.0)
+    #     return self._pdf(x, *args, baseloc, basescale)
 
-    def cdf(self, x, *args, **kwargs):
-        return self._cdf(x, *args, **kwargs)
+    # def cdf(self, x, *args, **kwargs):
+    #     baseloc=kwargs.get('baseloc', 0.0)
+    #     basescale=kwargs.get('basescale', 1.0)
+    #     return self._cdf(x, *args, baseloc, basescale)
     
     def _fitstart(self, data, args=None):
-        return self.basedist._fitstart(data, args=args)
+        theta_0 = self.basedist._fitstart(data, args=args)
+        return theta_0
 
     def freeze(self, *args, **kwds):
         return rv_wickselled_frozen(self, *args, **kwds)
