9540501b52e4902d4c5b62f5ad92fcdb838a51cc
Dorian Depriester <dorian@crans.org>
2020-09-09 16:01:50 +0200

    Implement get_support() and _ppf() methods

--- posnorm.py
+++ posnorm.py
@@ -19,5 +19,14 @@ class posnorm_gen(stats.rv_continuous):
     def _fitstart(self, data, args=None):
         return np.mean(data), np.std(data), 0.0, 1.0
 
+    def _get_support(self, *args, **kwargs):
+        return (0.0, np.inf)
+
     def _argcheck(self, *args):
         return args[1] > 0
+
+    def _ppf(self, q, mu, s):
+        a = -mu / s
+        A = (1 - stats.norm.cdf(a)) * q + stats.norm.cdf(a)
+        return s * stats.norm.ppf(A) + mu
+
