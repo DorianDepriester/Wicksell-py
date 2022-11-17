482304362f53b2b03ddf4e342bfa68185d700d29
Dorian Depriester <dorian@crans.org>
2022-11-17 16:03:04 +0100

    Remove unnecessary (redundant) arguments Add docstring

--- lognormal_uncertainty.py
+++ lognormal_uncertainty.py
@@ -1,30 +1,40 @@
 import numpy as np
 
-valid_params_msg = 'E, mu or scale.'
 
+def uncertainty(n, sigma, scale=1):
+    """
+    Computes the uncertainty on the estimator given by Maximum Likelihood Estimation (MLE) on the Wicksell transform of
+    lognormal distribution.
 
-def uncertainty(sigma, n, **kwargs):
-    if len(kwargs) == 0:
-        E = 1
-    elif len(kwargs) > 1:
-        raise TypeError('Only one optional argument is valid. It can be either ' + valid_params_msg)
-    else:
-        keyword, value = kwargs.popitem()
-        if keyword == 'E':
-            E = value
-        elif keyword == 'mu':
-            mu = value
-            E = np.exp(mu + sigma**2 / 2)
-        elif keyword == 'scale':
-            return uncertainty(sigma, n, mu=np.log(value))
-        else:
-            raise TypeError('Unknown optional argument. It can be ' + valid_params_msg)
+    Parameters
+    ----------
+    n : int
+        Sample size
+    sigma : float
+        Shape parameter of the lognormal distribution, given by MLE.
+    scale : float, optional
+        Scale option of the lognormal distribution. The default is 1.
+
+    Returns
+    -------
+    Dict
+        A dictionary with confidence intervals for the shape parameter (sigma), the expectation (E) and the log-scale
+        factor (mu).
+
+    Reference
+    ---------
+    Depriester D. and Kubler R. (2019), doi:10.5566/ias.2133
+
+    """
+
+    mu = np.log(scale)
+    E = np.exp(mu + sigma**2/2)
     sigma_min = sigma / (1 + 2.402/np.sqrt(n))
     sigma_max = sigma / (1 - 2.402/np.sqrt(n))
     E_min = E / (1 + 3.003 * sigma_max / np.sqrt(n))
     E_max = E / (1 - 3.003 * sigma_max / np.sqrt(n))
     mu_min = np.log(E_min) - sigma_max**2 / 2
     mu_max = np.log(E_max) - sigma_min**2 / 2
-    return {'sigma':(sigma_min, sigma_max),
-            'E':(E_min, E_max),
-            'mu':(mu_min, mu_max)}
+    return {'sigma': (sigma_min, sigma_max),
+            'E': (E_min, E_max),
+            'mu': (mu_min, mu_max)}
