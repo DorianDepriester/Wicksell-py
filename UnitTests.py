fc3751ff1b6a4d3c98b3de483dfe2a4e00ce4118
Dorian Depriester <dorian@crans.org>
2022-11-30 15:09:14 +0100

    Update docstrings

--- UnitTests.py
+++ UnitTests.py
@@ -1,8 +1,9 @@
 import scipy.stats as stats
 import numpy as np
-from Wicksell.compute_transform import from_continuous
+from Wicksell import compute_transform as wt
 from matplotlib import pyplot as plt
 from posnorm import posnorm_gen
+import histogram_tools as ht
 
 """
 The script will run 4 tests, investigating different base distributions (namely: uniform, positive normal, 
@@ -47,8 +48,16 @@ distros = {
          "fit_option": {'floc': 0}},
 }
 
+styles = {
+    "basedist": {"color": 'black',                      "label": 'Real (target) distr.'},
+    "two_step": {"color": 'red',                        "label": 'Two-step method'},
+    "fit":      {'color': 'b', 'linestyle': 'dotted',   "label": 'Fit (from distribution)'},
+    "fit_histogram": {"color": 'blue',                  "label": 'Fit (from histogram)'},
+    "Saltykov": {"color": 'red',                        "label": 'Saltykov method'}
+}
 
-def run_test(distributions=None, fit=True, kstest=True, nsample=1000):
+
+def run_test(distributions=None, two_step=True, fit_distribution=True, fit_histogram=True, kstest=True, nsample=1000):
     """
     Run tests on the distributions given above. By default, all the distributions will be considered, and the following
     test will be performed on each:
@@ -63,7 +72,7 @@ def run_test(distributions=None, fit=True, kstest=True, nsample=1000):
     distributions : list, optional
         List of the names of the investigated parameters. They can be 'uniform', 'positivenorm' of 'lognorm' and
         'weibull'.
-    fit : bool
+    fit_distribution : bool
         Turn on/off the fit step. Default is True
     kstest : bool
         Turn on/off the KS goodness-of-fit test. Requires fit=True. Default is True
@@ -74,20 +83,18 @@ def run_test(distributions=None, fit=True, kstest=True, nsample=1000):
         distributions = distros.keys()
 
     # Plotting options
-    x = np.linspace(0, 3.5, 1000)   # Used for plotting the PDFs
-    fig, axs = plt.subplots(len(distributions), 1, figsize=(8.27, 11.69))
-    if len(distributions) == 1:
-        axs = [axs]   # If only one subplot is used, it should still be subscriptable.
-    fig.tight_layout(h_pad=3)
+    x = np.linspace(0, 3.5, 1000)  # Used for plotting the PDFs
 
     for i, dist in enumerate(distributions):
+        fig, (ax_basedist, ax_transformed) = plt.subplots(2, 1, constrained_layout=True)
 
         # Compute PDF and transformed PDF
         basedist = distros[dist]['distro']
-        trans_dist = from_continuous(basedist)
+        trans_dist = wt.from_continuous(basedist)
         loc = distros[dist]['loc']
         scale = distros[dist]['scale']
         param = distros[dist]['param']
+        fit_option = distros[dist]['fit_option']
         if len(param):
             formatted_params = "{}, loc={}, scale={}".format(', '.join(map(str, param)), loc, scale)
         else:
@@ -97,41 +104,75 @@ def run_test(distributions=None, fit=True, kstest=True, nsample=1000):
         pdf = basedist.pdf(x, *param, loc=loc, scale=scale)
         tpdf = trans_dist.pdf(x, *param, loc=loc, scale=scale)
 
-        # Generate random data and plot them as histogram
+        # Generate random data
         sample = trans_dist.rvs(*param, loc=loc, scale=scale, size=nsample)
-        axs[i].hist(sample, ec='yellow', fc='orange', bins=25, density=True, label='Rand. samp.')
 
-        # Plot PDFs
-        axs[i].set_ylim(bottom=0.0, top=1.1 * max(pdf))
-        axs[i].set_xlim(left=0.0, right=3.5)
-        axs[i].plot(x, pdf, 'r', label='PDF')
-        axs[i].plot(x, tpdf, 'b', label='transf. PDF')
+        # Set up the bins here so that they are consistent en each bar plot
+        bins = np.linspace(0, 1.3 * max(sample), 21)
+
+        # Plot underlying/unfolded distributions
+        ax_basedist.set_ylim(bottom=0.0, top=1.1 * max(pdf))
+        ax_basedist.set_xlim(left=0.0, right=3.5)
+        style = {'color': 'black'}
+        ax_basedist.plot(x, pdf, label='Underlying PDF', **style)
+        ax_transformed.plot(x, tpdf, label='transf. PDF', **style)
+
+        # Plot transformed distributions and apparent histogram
+        ax_transformed.set_ylim(bottom=0.0, top=1.1 * max(tpdf))
+        ax_transformed.set_xlim(left=0.0, right=3.5)
+        ax_transformed.hist(sample, ec='yellow', fc='orange', bins=bins, density=True, label='Rand. samp.')
+
+        if two_step:
+            theta, hist_salt = ht.two_step_method(sample, basedist, bins=bins)
+            color = 'red'
+            label = 'Two-step method'
+            ht.plot_histogram(ax_basedist, hist_salt, ec=color, fc=color, alpha=0.2, label='Saltykov method')
+            ax_basedist.plot(x, basedist.pdf(x, *theta), color=color, label=label)
+            trans_hist_salt = wt.from_histogram(hist_salt)
+            ax_transformed.plot(x, trans_hist_salt.pdf(x), color=color, label=label, linestyle='dashdot')
 
         # Estimate the distribution parameter from the sample
-        if fit:
-            fit_option = distros[dist]['fit_option']
+        if fit_distribution:
             theta = trans_dist.fit(sample, **fit_option)
             print("Fit: {}".format(theta))
 
             # Plot fitted curve
-            axs[i].plot(x, trans_dist.pdf(x, *theta), 'b', linestyle='dotted', label='Fit')
+            style = {'label': 'Fit (from distribution)', 'color': 'b', 'linestyle': 'dotted'}
+            ax_transformed.plot(x, trans_dist.pdf(x, *theta), **style)
+            ax_basedist.plot(x, basedist.pdf(x, *theta), **style)
 
             # Print results and perform KS test
             if kstest:
                 ks = stats.kstest(sample, trans_dist.cdf, theta)
                 print('KS test: {}'.format(ks))
 
+        # Unfold the histogram
+        if fit_histogram:
+            # Unfold the distribution (w/o considering the continuous distribution)
+            hist, res = ht.fit_histogram(sample, bins=bins)
+            trans_hist = wt.from_histogram(hist)
+            color = 'blue'
+            label = 'Fit (from histogram)'
+            ht.plot_histogram(ax_basedist, hist, ec=color, fc=color, alpha=0.2, label=label)
+            ax_transformed.plot(x, trans_hist.pdf(x), color=color, label=label, linestyle='dashed')
+
         # Plot all of this
-        axs[i].set_xlabel('R')
-        axs[i].set_ylabel('Frequency')
-        axs[i].legend()
-        axs[i].set_title(dist)
+        fig.suptitle(dist, fontsize=16)
+        ax_transformed.set_xlabel('r')
+        ax_transformed.set_ylabel('Frequency')
+        ax_transformed.legend()
+        ax_transformed.set_title('Apparent/transformed distribution')
+
+        ax_basedist.set_xlabel('R')
+        ax_basedist.set_ylabel('Frequency')
+        ax_basedist.legend()
+        ax_basedist.set_title('Underlying/unfolded distributions')
+
+        fig.show()
         print("---")
 
     plt.show()
 
 
 if __name__ == "__main__":
-    run_test()
-
-
+    run_test(nsample=1000)
